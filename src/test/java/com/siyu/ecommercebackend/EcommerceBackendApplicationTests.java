db0e1f6002702dac998b67ffa1f1ed7afd2bd0e3
JohnsonL-G8 <liusiyug8@gmail.com>
2023-08-20 09:11:17 +0800

    First Commit

---
+++ src/test/java/com/siyu/ecommercebackend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siyu.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
